24851c7630713463b9da3feabe60e6be5bde310b
Steve Green <steve@greengob.com>
2023-12-17 21:46:25 +0000

    css table

--- background.js
+++ background.js
@@ -17,24 +17,6 @@ chrome.tabs.onUpdated.addListener(async () => {
   if (tab.url && tab.url.includes(rightmove + "/properties")) {
     const propertyID = tab.url.split("#")[0].slice(-9)
 
-    console.log(tab.url)
-    console.log(tab.id)
-    console.log(propertyID)
-    // chrome.tabs.sendMessage(tab.id, {
-    //   type: "NEW",
-    //   propertyID: propertyID
-    // })
-
-    // chrome.tabs.sendMessage(tab.id, { method: "updatePrice", propertyID: propertyID}, (response) => {
-    //   console.log(response);
-    // });
-    //
-    // chrome.runtime.sendMessage('updatePrice', (response) => {
-    //   // 3. Got an asynchronous response with the data from the service worker
-    //   console.log('received user data', response);
-    //   return true
-    // });
-
    chrome.tabs.sendMessage(tab.id, {
       type: "updatePrice",
       propertyID: propertyID
--- content-script.js
+++ content-script.js
@@ -4,7 +4,7 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     try {
       const response = await getHousePriceHistory(message.propertyID, price)
       let {data} = await response.json();
-      console.log(data)
+      displayPriceHistory(data)
       sendResponse(data)
     } catch (error) {
       console.error(error);
@@ -13,14 +13,69 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   }
 });
 
+function generateTableHead(table) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+
+  let priceColumn = document.createElement("th");
+  let priceColumnText = document.createTextNode("Price");
+  priceColumn.appendChild(priceColumnText);
+
+  let dateColumn = document.createElement("th");
+  let dateColumnText = document.createTextNode("Date");
+  dateColumn.appendChild(dateColumnText);
+
+  row.appendChild(dateColumn);
+  row.appendChild(priceColumn);
+}
+
+function generateTable(table, data) {
+  for (let element of data) {
+    const row = table.insertRow();
+
+    const dateCell = row.insertCell();
+    const date = new Date(element.date);
+    date.toLocaleDateString('en-GB')
+
+    const yyyy = date.getFullYear();
+    let mm = date.getMonth() + 1; // month is zero-based
+    let dd = date.getDate();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+
+    const formatted = dd + '/' + mm + '/' + yyyy;
+
+    const dateText = document.createTextNode(formatted);
+    dateCell.appendChild(dateText);
+
+    const priceCell = row.insertCell();
+
+    const quid = new Intl.NumberFormat('en-GB', {
+      style: 'currency',
+      currency: 'GBP',
+      maximumSignificantDigits: 4,
+    });
+
+    const priceText = document.createTextNode(quid.format(element.price));
+    priceCell.appendChild(priceText);
+  }
+}
+
+function displayPriceHistory(data) {
+  const parent = document.querySelector('main > div > *:last-child > div > article > div > div')
+  const table = document.createElement("table");
+  table.classList.add("styled-table");
+
+  generateTableHead(table);
+  generateTable(table, data);
+
+  parent.insertBefore(table, parent.lastChild)
+}
+
 function getHousePrice() {
-  const documentRoot = document.getElementById("root").innerHTML
-  console.log(documentRoot)
-  const parser = new DOMParser();
-    let res = parser.parseFromString(documentRoot, "text/html")
-  console.log(res)
-  // return document.getElementsByClassName("_1gfnqJ3Vtd1z40MlC0MzXu")[0].innerText;
-  return 300000
+  const value = document.getElementById("propertyValue").value
+  return Number(value.replace(/,/g, ''));
 }
 
 async function getHousePriceHistory(propertyID, price){
